2649372249ad2260833144f97e0b31b50756e797
raisameneses <raisameneses1@gmail.com>
2018-08-01 10:00:41 -0700

    fad-in / fade-out

--- app/src/main/java/com/raisa/animations/MainActivity.java
+++ app/src/main/java/com/raisa/animations/MainActivity.java
@@ -10,11 +10,18 @@ public class MainActivity extends AppCompatActivity {
 
     public  void fadeOut(View view){
         ImageView bart = (ImageView)findViewById(R.id.bartImage);
-        bart.animate().alpha(0.0f).setDuration(5000);
         ImageView homer = (ImageView)findViewById(R.id.homerImage);
-        homer.animate().alpha( 1.0f ).setDuration( 5000 );
+        if(bart.getAlpha() == 1.0){
+            bart.animate().alpha(0.0f).setDuration(5000);
+            homer.animate().alpha( 1.0f ).setDuration( 5000 );
+        } else {
+            homer.animate().alpha( 0.0f ).setDuration( 5000 );
+            bart.animate().alpha(1.0f).setDuration(5000);
+        }
 
     }
+
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
